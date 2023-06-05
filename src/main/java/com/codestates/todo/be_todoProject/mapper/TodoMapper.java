b065b7b5a32b6d9fe51b81d1e6d67f36757dad63
Mason3144 <lts890303@gmail.com>
2023-06-05 11:27:44 +0900

    Define Todo Mapper

---
+++ src/main/java/com/codestates/todo/be_todoProject/mapper/TodoMapper.java
@@ -0,0 +1,13 @@
+package com.codestates.todo.be_todoProject.mapper;
+
+import com.codestates.todo.be_todoProject.dto.TodoDto;
+import com.codestates.todo.be_todoProject.entity.Todos;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TodoMapper {
+    Todos todoDtoPostToTodos(TodoDto.Post todoDtoPost);
+    Todos todoDtoPatchToTodos(TodoDto.Patch todoDtopatch);
+    TodoDto.Response todosToTodoDtoResponse(Todos todos);
+}
