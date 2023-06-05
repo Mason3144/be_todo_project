45c42f05e1cbcbd406231807b5844159abda7313
Mason3144 <lts890303@gmail.com>
2023-06-05 10:32:41 +0900

    git init

---
+++ src/main/java/com/codestates/todo/be_todoProject/BeTodoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.todo.be_todoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTodoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTodoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/todo/be_todoProject/BeTodoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.todo.be_todoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTodoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
